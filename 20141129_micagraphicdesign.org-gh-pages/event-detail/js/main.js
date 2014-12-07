70d8909598a918d8d916955919d64c4744cb292c
cmuething <cmuething@mica.edu>
2014-12-07 16:59:36 -0500

    General Sync - 120714

---
+++ 20141129_micagraphicdesign.org-gh-pages/event-detail/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+  $('#utility-about').on('click', function (event) {
+    event.preventDefault();
+
+    $('html').toggleClass('js-about-active');
+  });
+});
+
+$(function () {
+  $('#utility-contact').on('click', function (event) {
+    event.preventDefault();
+
+    $('html').toggleClass('js-contact-active');
+  });
+});
+
+$(function () {
+  $('#utility-subscribe').on('click', function (event) {
+    event.preventDefault();
+
+    $('html').toggleClass('js-subscribe-active');
+  });
+});
+
+$(function () {
+  $('#utility-archives').on('click', function (event) {
+    event.preventDefault();
+
+    $('html').toggleClass('js-archives-active');
+  });
+});
+
+$(function () {
+  $('#utility-login').on('click', function (event) {
+    event.preventDefault();
+
+    $('html').toggleClass('js-login-active');
+  });
+});
+
+$(function () {
+  $('#utility-search').on('click', function (event) {
+    event.preventDefault();
+
+    $('html').toggleClass('js-search-active');
+  });
+});
