0983e333156301be0bd442445d513537ce7a9b0a
cmuething <cmuething@mica.edu>
2015-11-13 16:37:21 -0500

    new work
    the menu’s slide in from the side, top, and bottom

--- portfolio_site_update/js/index.js
+++ portfolio_site_update/js/index.js
@@ -29,3 +29,78 @@ $('#limit-r').click(function () {
         }
     });
 });
+
+//INFO
+
+$(document).ready(function () {
+    $('.connor-info-toggle').on('click', function(event){
+    	event.preventDefault();
+    	// create menu variables
+    	var connorInfo = $('.connor-info');
+    	var connorInfoWidth = $('.connor-info').width();
+    	
+    	// toggle open class
+    	connorInfo.toggleClass("open");
+    	
+    	// slide menu
+    	if (connorInfo.hasClass("open")) {
+	    	connorInfo.animate({
+		    	left: "0px"
+	    	});	
+    	} else {
+	    	connorInfo.animate({
+		    	left: -connorInfoWidth
+	    	}, 250);	
+    	}
+    });
+});
+
+//OVERVIEW
+
+$(document).ready(function () {
+    $('.overview-toggle').on('click', function(event){
+    	event.preventDefault();
+    	// create menu variables
+    	var overviewTop = $('.overview');
+    	var overviewTopHeight = $('.overview').height();
+    	
+    	// toggle open class
+    	overviewTop.toggleClass("open");
+    	
+    	// slide menu
+    	if (overviewTop.hasClass("open")) {
+	    	overviewTop.animate({
+		    	top: "0px"
+	    	});	
+    	} else {
+	    	overviewTop.animate({
+		    	top: -overviewTopHeight
+	    	}, 250);	
+    	}
+    });
+});
+
+//PROJECT DETAILS
+
+$(document).ready(function () {
+    $('.project-details-toggle').on('click', function(event){
+    	event.preventDefault();
+    	// create menu variables
+    	var projectBottom = $('.project-details');
+    	var projectBottomHeight = $('.project-details').height();
+    	
+    	// toggle open class
+    	projectBottom.toggleClass("open");
+    	
+    	// slide menu
+    	if (projectBottom.hasClass("open")) {
+	    	projectBottom.animate({
+		    	bottom: "0px"
+	    	});	
+    	} else {
+	    	projectBottom.animate({
+		    	bottom: -projectBottomHeight
+	    	}, 250);	
+    	}
+    });
+});
